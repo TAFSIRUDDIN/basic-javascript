f702f59c70c22a99c9c585a2d6694c0343feb3d3
Tafsir uddin <tafsirtapu04@gmail.com>
2021-01-22 01:42:11 +0600

    third problem

--- assignment.js
+++ assignment.js
@@ -3,13 +3,13 @@
 function kilometerToMeter(kilometer) {
    if(kilometer < 0){
       var meter = "Kilometer can not negetive value.(Thank You)";
-      return meter;
+   
    }
    else{
       var meter = kilometer * 1000;   /* we know 1km = 1000m*/
-      return meter;
+     
    }
-   
+   return meter;
 }
 
 // All expected output
@@ -26,6 +26,7 @@ console.log(thirdMan);
 
 
 
+
 //budgetCalculator problem solving
 
 function budgetCalculator(clock, phone, laptop){
@@ -34,26 +35,85 @@ function budgetCalculator(clock, phone, laptop){
    
    if(clock <  0 || phone < 0 || laptop < 0){
          var totalPrice = errormessege;
-         return totalPrice;
+      
    }
    else if(clock ==  0 && phone == 0 && laptop == 0){
          var totalPrice = errormessege1;
-         return totalPrice;
+        
    }
    else{
          var totalClockPrice = clock * 50;
          var totalPhonePrice = phone * 100;
          var totalLaptopPrice = laptop * 500;
          var totalPrice = totalClockPrice+ totalPhonePrice + totalLaptopPrice;
-         return totalPrice;
+         
    }
+   return totalPrice;
 }
 // All expected output
+//// Ask first man
 var firstManBudgetRange = budgetCalculator(20, 10, 2);
 console.log(firstManBudgetRange);
+// Ask second man
 var secondManBudgetRange = budgetCalculator(20, -10, 2);
 console.log(secondManBudgetRange);
+// Ask third man
 var thirdManBudgetRange = budgetCalculator(0, 10, 2);
 console.log(thirdManBudgetRange);
+// Ask fourth man
 var fourthManBudgetRange = budgetCalculator(0, 0, 0);
 console.log(fourthManBudgetRange);
+
+
+
+// hotelcost problem solving
+
+function hotelCost(day){
+   var totalCost = 0;
+   var messege = 'Quentity of days dose not negetive value. (Thank You)';
+   var messege1 = 'They are not stay hare for a single days. (Thank You)';
+   if(day < 0){
+      totalCost = messege;
+   }
+   else if(day == 0){
+      totalCost = messege1;
+   }
+   else{
+         if(day<=10){
+            totalCost = day * 100;
+         }
+         else if(day <=20){
+            var basic = 10 * 100;
+            var extraDay = day - 10;
+            var offerOne = extraDay * 80;
+            totalCost = basic + offerOne;
+         }
+         else{
+            var basic = 10 * 100;
+            var offerOne = 10 * 80;
+            var extraDay = day - 20;
+            var offerTwo = extraDay * 50;
+            totalCost = basic + offerOne + offerTwo;
+         }
+   }
+   return totalCost;
+}
+//All expected output
+//First man
+var firstMan = hotelCost(-10);
+console.log(firstMan);
+//Second man
+var secondMan = hotelCost(0);
+console.log(secondMan);
+//third man
+var thirdMan = hotelCost(10);
+console.log(thirdMan);
+//Fourth man
+var fourthMan = hotelCost(17);
+console.log(fourthMan);
+//Fifth man
+var fifthMan = hotelCost(20);
+console.log(fifthMan);
+//Sixth man
+var sixthMan = hotelCost(71);
+console.log(sixthMan);
